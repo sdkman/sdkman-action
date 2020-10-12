9a0adea0acdc99c8ca7f2a637e20d3da64683326
Eddú Meléndez <eddu.melendez@gmail.com>
2020-10-11 20:29:47 -0500

    Fix folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bash -c "source /opt/.sdkman/bin/sdkman-init.sh && sdk install $1 $2"
-bash -c "mkdir -p /github/workspace/"
+bash -c "mkdir -p /github/workspace/.sdkman/archives/"
 bash -c "cp -r /opt/.sdkman/archives /github/workspace/.sdkman/"
 file=$(ls /github/workspace/.sdkman/archives | head -1)
 echo "::set-output name=file::$file"
